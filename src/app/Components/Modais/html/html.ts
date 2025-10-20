2de5f7edc165bbf8e50b12750ff7a11ee717d9f1
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-10-20 16:58:56 -0300

    Iniciando o portfólio, com as informações e os planetas finalizados.

---
+++ src/app/Components/Modais/html/html.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-html',
+  imports: [],
+  templateUrl: './html.html',
+  styleUrl: './html.css'
+})
+export class HTML {
+
+}
