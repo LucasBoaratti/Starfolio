bc323815de5af6004b976a8076c00f07b8cb6bc8
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-10-24 16:45:20 -0300

    Criação do rodapé.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from "./Components/Cabecalho/cabecalho";
+import { Rodape } from "./Components/rodape/rodape";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho],
+  imports: [RouterOutlet, Cabecalho, Rodape],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
