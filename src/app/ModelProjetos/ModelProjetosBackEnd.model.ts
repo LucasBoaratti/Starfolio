da5157441047f3dea28dc2af5c58f5ade972a68d
Lucas Boaratti <lucasboaratti15@gmail.com>
2026-03-21 02:56:56 -0300

    Adicionando novos projetos de FrontEnd.

--- src/app/ModelProjetos/ModelProjetosBackEnd.model.ts
+++ src/app/ModelProjetos/ModelProjetosBackEnd.model.ts
@@ -6,7 +6,6 @@ export const projetosBackEnd = [
         "holocronSith": "assets/images/Holocron_Sith.png",
         "logoProjeto": "assets/images/Logo_Jokempô.png",
         "descricaoProjeto": "Jokempô, um jogo sensacional jogado em toda a galáxia... É muito simples de jogar, pois você só precisa fazer um sinal de 'soco', 'palma', e um 'dois' com os dedos/mãos e tentar a vitória contra outra pessoa. Mas isso é na vida real, e se você não tiver amigos que nem eu? Pois então, nesse programa feito em python, você pode jogar contra o computador e quem fizer 15 pontos primeiro, vence a partida!",
-        // "video": "assets/videos/MineLucas.mp4",
         "ferramentas": [
             "assets/images/Icone_Python.png",
         ],
@@ -17,7 +16,6 @@ export const projetosBackEnd = [
         "holocronSith": "assets/images/Holocron_Sith.png",
         "logoProjeto": "assets/images/Logo_Adivinhucas.png",
         "descricaoProjeto": "A Adivinhucas é um jogo realizado em python que pode ser jogado em toda a galáxia! É um jogo onde você tem que adivinhar o número adivinhado pelo computador em até 15 tentativas. A dica que eu já dou é que o número vai até 50 ou seja, a dificuldade está em um nível médio para você acertar.",
-        // "video": "assets/videos/MineLucas.mp4",
         "ferramentas": [
             "assets/images/Icone_Python.png",
         ],
@@ -28,7 +26,6 @@ export const projetosBackEnd = [
         "holocronSith": "assets/images/Holocron_Sith.png",
         "logoProjeto": "assets/images/Logo_DownLucas.png",
         "descricaoProjeto": "A DownLucas é um site muito maneiro, pois, você já viu um vídeo de um certo planeta ou de outro lugar da galáxia no aplicativo YouTube e você queria baixá-lo, mas tinha que assinar o YouTube Premium e não tinha dinheiro? Pois na DownLucas, você só vai precisar do link do vídeo e baixá-lo em casa, sem precisar assinar nada! Isso mesmo, aqui é possível.",
-        // "video": "assets/videos/MineLucas.mp4",
         "ferramentas": [
             "assets/images/Icone_Python.png",
         ],
@@ -39,7 +36,6 @@ export const projetosBackEnd = [
         "holocronSith": "assets/images/Holocron_Sith.png",
         "logoProjeto": "assets/images/Logo_BlocoAPI.png",
         "descricaoProjeto": "Na BlocoAPI, você pode criar blocos do Minecraft e deixá-los exibidos em sua própria API! O interessante é que junta a nossa galáxia de Star Wars, com um lindo mundo do Minecraft com diversos blocos coloridos e bonitos para você se encantar. Deu trabalho, mas compensou muito, pois tive que explorar o difícil planeta chamado Kessel, e ainda consegui bater o recorde de realizar o percurso de Kessel em 10 parsecs (minutos).",
-        // "video": "assets/videos/MineLucas.mp4",
         "ferramentas": [
             "assets/images/Icone_Python.png",
             "assets/images/Icone_FastAPI.png",
@@ -51,7 +47,6 @@ export const projetosBackEnd = [
         "holocronSith": "assets/images/Holocron_Sith.png",
         "logoProjeto": "assets/images/Logo_BlocoAPI.png",
         "descricaoProjeto": "Na BlocoAPI, você pode criar blocos do Minecraft e deixá-los exibidos em sua própria API! O interessante é que junta a nossa galáxia de Star Wars, com um lindo mundo do Minecraft com diversos blocos coloridos e bonitos para você se encantar. Deu trabalho, mas compensou muito, pois tive que explorar o difícil planeta chamado Kessel, e ainda consegui bater o recorde de realizar o percurso de Kessel em 10 parsecs (minutos).",
-        // "video": "assets/videos/MineLucas.mp4",
         "ferramentas": [
             "assets/images/Icone_Python.png",
             "assets/images/Icone_FastAPI.png",
